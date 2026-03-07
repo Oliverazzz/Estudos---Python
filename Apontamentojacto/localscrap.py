b8ed668d8a776ba078563378995876799eb28f5a
Luiz Oliveira <luizfoliveirah@gmail.com>
2026-03-07 00:45:43 -0300

    save lógica scrap

--- Apontamentojacto/localscrap.py
+++ Apontamentojacto/localscrap.py
@@ -5,11 +5,9 @@ from pathlib import Path
 import xml.etree.ElementTree as et
 
 
-
-def xmlscrap(nprog):
+def checkout(nprog):
 
     #tratamento de str (bugs por conta de "\" no caminho)
-    nprog = str(nprog)
     file = str(r"\\jacvmprdft02\program$\3030\\")
     file = file[:-1]
 
@@ -21,7 +19,54 @@ def xmlscrap(nprog):
         nprog = "L" + nprog
     prog = str(file.strip() + nprog + ".xml")
     
-    if not os.path.exists
+    if not os.path.exists(prog):
+        return "Programa não existe"
+    else:
+        try:
+            tree = et.parse(prog, parser="etree")
+            root = tree.getroot()
+            return root
+        except Except as e:
+            erro = type(e).__name__
+            return f"Erro: {e}"
+
+    response = scrap(root)
+    return response
 
 
-n = xmlscrap("15413079")
+def scrap(root): #scrap no xml (Dms, ordens, blank minimo, material, dimensão por peça)
+
+    
+    dmsall = root.findall(".//PartoNo") #encontra todos os DMS das peças
+    listdms = []
+    for dms in dmsall: 
+        dms = dms.text                 #Formarta os DMS
+        indexdms = dms.find("-")
+        dms = dms[(indexdms + 1):]
+        listdms.append(dms)
+
+    
+    orderall = root.findall(".//CustomerName") #encontra todas as ordens das peças
+    listorders = []
+    for order in orderall:
+        order = order.text
+        lastindex = 0 
+        count = order.count(",")
+        orderunit = []
+        for a in range (0, (count + 1)):
+            if (a <= count):
+                orderindex = order.find(lastindex, ",")
+                order1 = order[lastindex:orderindex].strip()
+                lastindex = orderindex + 1
+                orderunit.append(order1) 
+        else:
+                order1 = [lastindex:]
+                orderunit.append(order1)
+        listorders.append(orderunit)
+            
+    return orderunit, listdms 
+                
+        
+    
+
+n = checkout(str("13131"))
