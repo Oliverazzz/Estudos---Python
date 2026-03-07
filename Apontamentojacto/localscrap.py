1ec1f9d6ee0b153c4581849c71a40d73c5d87e85
Luiz Oliveira <luizfoliveirah@gmail.com>
2026-03-07 00:55:06 -0300

    save

--- Apontamentojacto/localscrap.py
+++ Apontamentojacto/localscrap.py
@@ -7,18 +7,18 @@ import xml.etree.ElementTree as et
 
 def checkout(nprog):
 
-    #tratamento de str (bugs por conta de "\" no caminho)
+    # tratamento de str (bugs por conta de "\" no caminho)
     file = str(r"\\jacvmprdft02\program$\3030\\")
     file = file[:-1]
 
-    #1. uppercase no caractere L no começo da str
-    #2. Formata o caminho completo do programa
+    # 1. uppercase no caractere L no começo da str
+    # 2. Formata o caminho completo do programa
     if nprog.strip().startswith("l"):
         nprog = nprog.capitalize()
     elif nprog[0] != "L":
         nprog = "L" + nprog
     prog = str(file.strip() + nprog + ".xml")
-    
+
     if not os.path.exists(prog):
         return "Programa não existe"
     else:
@@ -34,52 +34,36 @@ def checkout(nprog):
     return response
 
 
-def scrap(root): #scrap no xml (Dms, ordens, blank minimo, material, dimensão por peça)
+def scrap(root):  # scrap no xml (Dms, ordens, blank minimo, material, dimensão por peça)
 
-    
-    dmsall = root.findall(".//PartoNo") #encontra todos os DMS das peças
+    dmsall = root.findall(".//PartoNo")  # encontra todos os DMS das peças
     listdms = []
-<<<<<<< HEAD
-    for dms in dmsall: 
-        dms = dms.text                 #Formarta os DMS
-        indexdms = dms.find("-")
-        dms = dms[(indexdms + 1):]
-=======
-    for dms in dmsall:                  #Formarta os DMS
+    for dms in dmsall:
+        dms = dms.text  # Formarta os DMS
         indexdms = dms.find("-")
         dms = dms[indexdms:]
->>>>>>> 009c3339bd17283ad7cefbcae8c8f22e4c52ddfb
         listdms.append(dms)
 
-    
-    orderall = root.findall(".//CustomerName") #encontra todas as ordens das peças
+    # encontra todas as ordens das peças
+    orderall = root.findall(".//CustomerName")
     listorders = []
     for order in orderall:
-<<<<<<< HEAD
         order = order.text
-=======
->>>>>>> 009c3339bd17283ad7cefbcae8c8f22e4c52ddfb
-        lastindex = 0 
+        lastindex = 0
         count = order.count(",")
         orderunit = []
-        for a in range (0, (count + 1)):
+        for a in range(0, (count + 1)):
             if (a <= count):
-                orderindex = order.find(lastindex, ",")
+                orderindex = order.find(",", lastindex)
                 order1 = order[lastindex:orderindex].strip()
                 lastindex = orderindex + 1
-                orderunit.append(order1) 
-        else:
-                order1 = [lastindex:]
                 orderunit.append(order1)
+        else:
+            order1 = order[lastindex:]
+            orderunit.append(order1)
         listorders.append(orderunit)
-            
-    return orderunit, listdms 
-                
-        
-    
 
-<<<<<<< HEAD
-n = checkout(str("13131"))
-=======
-n = checkout(str("13131"))
->>>>>>> 009c3339bd17283ad7cefbcae8c8f22e4c52ddfb
+    return orderunit, listdms
+
+
+n = checkout("13131")
