00222a4eb0cac8c25b4502ace3067164de136b18
Luiz Oliveira <luizfoliveirah@gmail.com>
2026-04-04 19:08:26 -0300

    tratamento de erros ao salvar dados, implementação em atualização de cadatros

---
+++ Apontamentojacto/localscrap.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import os
+import json
+from pathlib import Path
+import xml.etree.ElementTree as et
+
+
+def checkout(nprog):
+
+    # tratamento de str (bugs por conta de "\" no caminho)
+    file = str(r"C:\Users\luizf\Desktop\.vscode\python\Apontamentojacto\\")
+    file = file[:-1]
+
+    # 1. uppercase no caractere L no começo da str
+    # 2. Formata o caminho completo do programa
+    if nprog.strip().startswith("l"):
+        nprog = nprog.capitalize()
+    elif nprog[0] != "L":
+        nprog = "L" + nprog
+    prog = str(file.strip() + nprog + ".xml")
+
+    if not os.path.exists(prog):
+        return "Programa não existe"
+    else:
+        try:
+            tree = et.parse(prog)
+            root = tree.getroot()
+        except Exception as e:
+            erro = type(e).__name__
+            return f"Erro: {e}"
+    return root
+
+
+def scrap(root):  # scrap no xml (Dms, ordens, blank minimo, material, dimensão por peça)
+
+    #Encontra todos os DMS
+    dmsall = root.findall(".//PartoNo")  # encontra todos os DMS das peças
+    listdms = []
+    for dms in dmsall:
+        dms = dms.text  # Formarta os DMS
+        indexdms = dms.find("-")
+        dms = dms[(indexdms + 1):]
+        listdms.append(dms)
+
+    
+    #Encontra todas as ordens das peças
+    orderall = root.findall(".//CustomerName")
+    listorders = []
+    for order in orderall:
+        order = order.text
+        lastindex = 0
+        count = order.count(",")
+        orderunit = []
+        for a in range(0, (count + 1)):
+            if (a <= count):
+                orderindex = order.find(",", lastindex)
+                order1 = order[lastindex:orderindex].strip()
+                lastindex = orderindex + 1
+                orderunit.append(order1)
+        else:
+            order1 = order[lastindex:]
+            orderunit.append(order1)
+        listorders.append(orderunit)
+
+    
+    # Blank mínimo
+    blankx = []
+    blanky = []
+    blankminx = root.findall(".//MinimumSheetSizeInX")
+    blankminy = root.findall(".//MinimumSheetSizeIny")
+    blankx = [i.stripe().text for i in blankminx if i not None]
+    blanky = [i.stripe().text for i in blankminy if i not None]
+    blankzip = zip(blankx, blanky)
+
+
+    #Dimensões por peça
+    dporpeca = []
+    l = root.findall(".//Length")
+    w = root.findall(".//Width")
+    for lenght, width in zip_longest(l, w, fillvalue=None)
+        dp = {
+            "Length":lenght,
+            "Width":width
+        }
+        dporpeca.append(dp) if not None
+    
+    
+
+    
+    #Material
+    material = root.find(".//DrawingNo")
+    material = material.text.strip() if material else return "Erro: código de material não encontrado"
+    
+
+
+   
+n = checkout("12257")
+response = scrap(n)
+print(response)
