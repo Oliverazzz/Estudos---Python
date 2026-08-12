82c6974c07e510d463734fa6f6d6a1cbb68f02ae
Luiz Oliveira <luizfoliveirah@gmail.com>
2026-08-12 15:11:03 -0300

    demonstração

--- Estudo/Python/Django/cars/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Cars(models.Model):
-    model = models.TextField(max_length=20)
-    price = models.TextField(max_length=20)
-    color = models.TextField(max_length=20)                                                         
--- Estudo/Python/Django/cars/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Cars
-
-# Create your views here.
-def cars(request):
-    cars = Cars.objects.all()
-
-    return render(request, 'cars/cars.html', {'cars': cars})
